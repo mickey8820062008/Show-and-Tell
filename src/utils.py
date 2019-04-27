5092e6fc5de3778f8943905dee92645fb85159b5
mickey8820062008 <mickey8820062008@gmail.com>
2019-04-27 12:31:17 -0500

    Add resolve_caption to utils.py

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[84]:
+
+
+import torch
+
+
+# In[86]:
+
+
+def resolve_caption(outputs, reference='./preprocessed_data/flickr8k_id_to_word.pylist'):
+    # outputs: (batch_size, timesteps, features)
+    id_to_word = torch.load(reference)
+    
+    captions = []
+    
+    for output in outputs:
+        caption = ' '.join([id_to_word[id] for id in output])
+        captions.append(caption)
+    # outputs: (batch_size)
+    return captions
+
